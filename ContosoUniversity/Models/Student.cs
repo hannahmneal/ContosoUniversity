5880d4238c91d4dd238d12d8fe6f20d1db1795d0
hanna <hannahmneal@gmail.com>
2019-04-22 02:36:56 -0500

    added annotations for datetime in Student.cs

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+//NOTE: This enables annotations, which we need for displaying specific data:
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
@@ -8,6 +10,11 @@ namespace ContosoUniversity.Models
         public int Id { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+
+        //NOTE: This annotations below (in brackets) will enable the datetime format to be shown as YYYY-MM-DD instead of YYYY-MM-DD 00:00:00:
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
